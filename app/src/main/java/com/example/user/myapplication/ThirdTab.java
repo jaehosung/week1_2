a10ba5c5b352e0b2de47e1cba146dc8aed5167c4
jaehosung <you@example.com>
2017-12-27 17:35:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user/myapplication/ThirdTab.java
@@ -0,0 +1,57 @@
+package com.example.user.myapplication;
+
+/*
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by user on 2017-12-26.
+ */
+
+
+/*
+public class FirstTab extends Activity{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        String activityName = getClass().getSimpleName();
+
+        TextView tv = new TextView(this);
+        tv.setText(activityName + "화면");
+        setContentView(tv);
+    }
+}
+
+package com.example.kjh.viewpager_fragment;
+*/
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class ThirdTab extends Fragment {
+    public ThirdTab()
+    {
+        // required
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        ConstraintLayout layout = (ConstraintLayout) inflater.inflate(R.layout.fragment3,
+                container, false);
+        return layout;
+    }
+}

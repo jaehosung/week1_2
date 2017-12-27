ac8ebaa41ff02a6437335ae86ed73ae664f3b095
jaehosung <you@example.com>
2017-12-27 20:25:58 +0900

    error...

--- app/src/main/java/com/example/user/myapplication/ThirdTab.java
+++ app/src/main/java/com/example/user/myapplication/ThirdTab.java
@@ -1,31 +1,5 @@
 package com.example.user.myapplication;
 
-/*
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-/**
- * Created by user on 2017-12-26.
- */
-
-
-/*
-public class FirstTab extends Activity{
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-
-        String activityName = getClass().getSimpleName();
-
-        TextView tv = new TextView(this);
-        tv.setText(activityName + "화면");
-        setContentView(tv);
-    }
-}
-
-package com.example.kjh.viewpager_fragment;
-*/
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
@@ -33,12 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class ThirdTab extends Fragment {
-    public ThirdTab()
-    {
-        // required
+    public ThirdTab() {
     }
 
     @Override
@@ -46,12 +21,49 @@ public class ThirdTab extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    @Nullable
+
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        ConstraintLayout layout = (ConstraintLayout) inflater.inflate(R.layout.fragment3,
-                container, false);
-        return layout;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        ConstraintLayout layout = (ConstraintLayout) inflater.inflate(R.layout.fragment3,container, false);
+
+        final EditText mMemoEdit = null;
+        final TextFileManager mTextFileManager = new TextFileManager(getActivity());
+
+        View view = inflater.inflate(R.layout.fragment3, container, false);
+
+
+        Button button1 = (Button)view.findViewById(R.id.load_btn);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String memoData = mTextFileManager.load();
+               mMemoEdit.setText(memoData);
+               Toast.makeText(getActivity(), "Successfully loaded", Toast.LENGTH_LONG).show();
+            }
+        });
+        Button button2 = (Button)view.findViewById(R.id.save_btn);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 String memoData = mMemoEdit.getText().toString();
+                mTextFileManager.save(memoData);
+                mMemoEdit.setText("");
+                Toast.makeText(getActivity(), "Successfully saved", Toast.LENGTH_LONG).show();
+            }
+        });
+        Button button3 = (Button)view.findViewById(R.id.delete_btn);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  mTextFileManager.delete();
+              //  mMemoEdit.setText("");
+                Toast.makeText(getActivity(), "Successfully deleted", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        return view;
     }
 }
